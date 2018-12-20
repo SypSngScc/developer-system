17d09f664dd262c27664672bd9f93e26136f63eb
shiyuping <shiyp@ngarihealth.com>
2018-12-20 16:17:02 +0800

    test

--- src/main/java/com/sss/DeveloperSystemApplication.java
+++ src/main/java/com/sss/DeveloperSystemApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeveloperSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeveloperSystemApplication.class, args);
+         SpringApplication.run(DeveloperSystemApplication.class, args);
     }
 
 }

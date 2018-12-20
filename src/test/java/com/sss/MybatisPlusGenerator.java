249a50f28267980f87bccf431fb3e01ff8fa216f
shiyuping <shiyp@ngarihealth.com>
2018-12-20 17:15:56 +0800

    add mybatispuls config

---
+++ src/test/java/com/sss/MybatisPlusGenerator.java
@@ -0,0 +1,122 @@
+package com.sss;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MybatisPlusGenerator {
+
+    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=utf8&autoReconnect=true";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "root";
+    private static final String ROOT = "D:\\IdeaProject\\test";
+    private static final String DIR = "";
+    private static final String BASE_PACKAGE = "com.sss";
+    private static final String AUTHOR = "syp";
+
+    @Test
+    public void generate() {
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(ROOT + DIR);
+        gc.setOpen(false);
+        gc.setFileOverride(false);
+        gc.setActiveRecord(true);
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(false);// XML columList
+        gc.setAuthor(AUTHOR);
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setMapperName("%sDao");
+        gc.setXmlName("%sMapper");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setControllerName("%sApiController");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUrl(DB_URL);
+        dsc.setUsername(DB_USER);
+        dsc.setPassword(DB_PASS);
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setTablePrefix(new String[]{"sys_", "t_"});// 此处可以修改为您的表前缀
+        strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
+        strategy.setInclude(new String[] { "t_store_manager" }); // 需要生成的表
+        // strategy.setExclude(new String[] { "zz_privilege", "zz_resource",
+        // "zz_role_resource", "zz_user_permission",
+        // "zz_user_resource" }); // 排除生成的表
+        // 字段名生成策略
+        strategy.setDbColumnUnderline(true);
+        // 自定义实体父类
+        // strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
+        // 自定义实体，公共字段
+        // strategy.setSuperEntityColumns(new String[] { "create_by",
+        // "create_at", "update_by", "update_at" });
+        // 自定义 mapper 父类
+        // strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
+        // 自定义 service 父类
+        // strategy.setSuperServiceClass("com.baomidou.demo.TestService");
+        // 自定义 service 实现类父类
+        // strategy.setSuperServiceImplClass("com.baomidou.demo.TestServiceImpl");
+        // 自定义 controller 父类
+        strategy.setSuperControllerClass("com.power.bos.utils.api.RestApiController");
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // strategy.setEntityColumnConstant(true);
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        // strategy.setEntityBuliderModel(true);
+        mpg.setStrategy(strategy);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent(BASE_PACKAGE);
+        // pc.setModuleName("");
+        pc.setController("controller");
+        mpg.setPackageInfo(pc);
+
+        // 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+                this.setMap(map);
+            }
+        };
+        mpg.setCfg(cfg);
+
+        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/template 下面内容修改，
+        // 放置自己项目的 src/main/resources/template 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+        TemplateConfig tc = new TemplateConfig();
+        tc.setController("/templates/api-controller.java.vm");
+        tc.setEntity("/templates/entity.java.vm");
+        tc.setMapper("/templates/mapper.java.vm");
+        // tc.setXml("...");
+        // tc.setService("...");
+        // tc.setServiceImpl("...");
+        mpg.setTemplate(tc);
+
+        // 执行生成
+        mpg.execute();
+
+        // 打印注入设置
+        System.err.println(mpg.getCfg().getMap().get("abc"));
+    }
+
+}
